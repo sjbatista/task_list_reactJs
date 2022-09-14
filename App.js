7e73cf8a148662c7e51a8dd46efd9016669e0e74
sjbatista <salomaojunio@hotmail.com>
2022-09-14 14:47:34 -0300

    added new fonts

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  BalsamiqSans_400Regular,
+  BalsamiqSans_400Regular_Italic,
+  BalsamiqSans_700Bold,
+  BalsamiqSans_700Bold_Italic,
+} from '@expo-google-fonts/balsamiq-sans';
 
 export default function App() {
+  
+  let [fontsLoaded] = useFonts({
+    BalsamiqSans_400Regular,
+    BalsamiqSans_400Regular_Italic,
+    BalsamiqSans_700Bold,
+    BalsamiqSans_700Bold_Italic,
+  });
 
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  const iconMinus = <AntDesign name="minuscircleo" size={24} color="black" />;
   const image = require('./resources/bg.jpg');
-    
+  
   return (
-
-    <View style={{flex:1}}>       
+    
+    <View style={{flex:1}}>
+    <StatusBar></StatusBar>       
     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
         <View style={styles.coverView}><Text style={styles.textHeader}>Task List</Text></View>
         
-    </ImageBackground>     
+    </ImageBackground>  
+     {iconMinus}
     </View>
   );
 
@@ -21,8 +44,7 @@ const styles = StyleSheet.create({
   image: {
     width:'100%',
     resizeMode:"cover",
-    height:80,
-
+    height:80
   },
   
   textBody: {
@@ -37,7 +59,8 @@ const styles = StyleSheet.create({
     fontSize:20,
     textAlign:'center',
     color:'white',
-    marginTop:20
+    marginTop:30,
+    fontFamily:'BalsamiqSans_700Bold_Italic'
   },
 
   coverView:{
