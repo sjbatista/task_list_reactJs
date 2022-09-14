8450b931b52716d0d3251541c9fe1413d212d8a8
sjbatista <salomaojunio@hotmail.com>
2022-09-14 12:49:43 -0300

    imageBackgroundComponent added

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
 
 export default function App() {
+
+  const image = require('./resources/bg.jpg');
+    
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={{flex:1}}>       
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.coverView}><Text style={styles.textHeader}>Task List</Text></View>
+        
+    </ImageBackground>     
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    width:'100%',
+    resizeMode:"cover",
+    height:80,
+
   },
+  
+  textBody: {
+    textAlign:'justify',
+  },
+
+  viewBody: {
+    alignItems:'center',
+  },
+
+  textHeader:{
+    fontSize:20,
+    textAlign:'center',
+    color:'white',
+    marginTop:20
+  },
+
+  coverView:{
+    width:'100%',
+    height:80,
+    backgroundColor:'rgba(0,0,0,0.3)',
+  }
+
 });
